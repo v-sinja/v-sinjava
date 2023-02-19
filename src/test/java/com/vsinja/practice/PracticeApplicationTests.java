dfe607f8dcbd769e83676d978afd01cac3bc4acd
DHY <pointrd87@gmail.com>
2023-02-19 12:57:52 +0000

    first commit

---
+++ src/test/java/com/vsinja/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsinja.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
